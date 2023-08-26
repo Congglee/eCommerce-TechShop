ad9870f312948cde00e32d99026b2fe0facabbe9
Cong Le <congldqn888@gmail.com>
2023-08-26 23:08:29 +0700

    fix: CORS stripe payment bug #2

--- server/src/main.js
+++ server/src/main.js
@@ -20,12 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const corsOptions = {
-  origin: ["https://ecommerce-techshop.vercel.app", "http://localhost:5173"], // or "*", to allow all origins
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use("/api", productRoute);
 app.use("/api", categoryRoute);
